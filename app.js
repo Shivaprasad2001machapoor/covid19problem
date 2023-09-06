890bccbcb98302c99f19e6dfe78680b27d7dabc1
shivaprasad <shivaprasadnomula75@gmail.com>
2023-09-06 09:35:47 +0000

    solution

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.use(express.json());
 const dbPath = path.join(__dirname, "covid19IndiaPortal.db");
 let db = null;
 
-const secretKey = "shivaprasadnomulahello"; // Replace with a secure secret key
-
 const initializeDBAndServer = async () => {
   try {
     db = await open({
@@ -45,7 +43,7 @@ app.post("/login", async (request, response) => {
       const payload = {
         username: username,
       };
-      const jwtToken = jwt.sign(payload, secretKey);
+      const jwtToken = jwt.sign(payload, "hsgdiedosjd");
       response.send({ jwtToken });
       response.status(200);
     } else {
@@ -56,25 +54,7 @@ app.post("/login", async (request, response) => {
 });
 
 // Authentication Middleware
-const authenticateUser = (req, res, next) => {
-  const token = req.header("Authorization");
-  if (!token) {
-    res.status(401).json("Invalid JWT Token");
-    return;
-  }
-
-  try {
-    const decoded = jwt.verify(token, secretKey);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(401).json("Invalid JWT Token");
-  }
-};
-
-//API 2
-
-app.get("/states/", (request, response) => {
+const authenticateToken = (request, response, next) => {
   let jwtToken;
   const authHeader = request.headers["authorization"];
   if (authHeader !== undefined) {
@@ -82,68 +62,209 @@ app.get("/states/", (request, response) => {
   }
   if (jwtToken === undefined) {
     response.status(401);
-    response.send("Invalid Access Token");
+    response.send("Invalid JWT Token");
   } else {
-    jwt.verify(jwtToken, secretKey, async (error, payload) => {
+    jwt.verify(jwtToken, "hsgdiedosjd", async (error, payload) => {
       if (error) {
-        response.send("Invalid Access Token");
+        response.status(401);
+        response.send("Invalid JWT Token");
       } else {
-        const getStatesQuery = `
+        request.username = payload.username;
+        next();
+      }
+    });
+  }
+};
+
+//chnage format
+
+const convertDirectorDbObjectToResponseObject = (eachState) => {
+  return {
+    stateId: eachState.state_id,
+    stateName: eachState.state_name,
+    population: eachState.population,
+  };
+};
+
+const convertDirectorDistrictToResponseObject = (eachDistrict) => {
+  return {
+    districtName: eachDistrict.district_name,
+    stateId: eachDistrict.state_id,
+    cases: eachDistrict.cases,
+    cured: eachDistrict.cured,
+    active: eachDistrict.active,
+    deaths: eachDistrict.deaths,
+  };
+};
+
+const convertDirectorDistrictsToResponseObject = (eachDistrict) => {
+  return {
+    districtId: eachDistrict.district_id,
+    districtName: eachDistrict.district_name,
+    stateId: eachDistrict.state_id,
+    cases: eachDistrict.cases,
+    cured: eachDistrict.cured,
+    active: eachDistrict.active,
+    deaths: eachDistrict.deaths,
+  };
+};
+
+//API 2
+
+app.get("/states/", authenticateToken, async (request, response) => {
+  const getStatesQuery = `
             SELECT
               *
             FROM
             state`;
-        const staesArray = await db.all(getStatesQuery);
-        response.send(statesArray);
-      }
-    });
+  const statesArray = await db.all(getStatesQuery);
+  response.send(
+    statesArray.map((eachState) =>
+      convertDirectorDbObjectToResponseObject(eachState)
+    )
+  );
+});
+
+//API 3
+
+app.get("/states/:stateId/", authenticateToken, async (request, response) => {
+  const { stateId } = request.params;
+  const getStateQuery = `
+            SELECT
+              *
+            FROM
+              state
+            WHERE 
+              state_id= ${stateId};`;
+  const stateArray = await db.get(getStateQuery);
+  response.send(convertDirectorDbObjectToResponseObject(stateArray));
+});
+
+//API 4
+
+app.get("/districts/", authenticateToken, async (request, response) => {
+  const getDistrictsQuery = `
+            SELECT
+              *
+            FROM
+            district`;
+  const districtsArray = await db.all(getDistrictsQuery);
+  response.send(
+    districtsArray.map((eachDistrict) =>
+      convertDirectorDistrictToResponseObject(eachDistrict)
+    )
+  );
+});
+
+//API 4
+
+app.post("/districts/", authenticateToken, async (request, response) => {
+  const distDetails = request.body;
+  const { districtName, stateId, cases, cured, active, deaths } = bookDetails;
+  const addDistQuery = `
+    INSERT INTO
+      book (district_name,state_id,cases,cured,active,deaths)
+    VALUES
+      (
+        '${districtName}',
+         ${authorId},
+         ${cases},
+         ${cured},
+         ${active},
+         ${deaths},
+      );`;
+
+  const dbResponse = await db.run(addDistQuery);
+  const DistId = dbResponse.lastID;
+  response.send("District Successfully Added");
+});
+
+//API 5
+
+app.get(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const getDistrictQuery = `
+            SELECT
+              *
+            FROM
+              district
+            WHERE 
+              district_id= ${districtId};`;
+    const districtArray = await db.get(getDistrictQuery);
+    response.send(convertDirectorDistrictsToResponseObject(districtArray));
   }
-});
+);
 
-// API 3: Get State by State ID
-app.get("/states/:stateId", authenticateUser, async (req, res) => {
-  const { stateId } = req.params;
-  const selectStateQuery = "SELECT * FROM state WHERE state_id = ?";
-  const state = await db.get(selectStateQuery, [stateId]);
-  if (!state) {
-    res.status(404).json("State not found");
-    return;
+//API 6
+
+app.delete(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const deleteDistrictQuery = `
+    DELETE FROM
+      district
+    WHERE
+      district_id = ${districtId};`;
+    await db.run(deleteDistrictQuery);
+    response.send("District Removed");
   }
-  res.json(state);
-});
+);
 
-// API 4: Create District
-app.post("/districts", authenticateUser, async (req, res) => {
-  const { districtName, stateId, cases, cured, active, deaths } = req.body;
+//API -7
 
-  const createDistrictQuery = `
-    INSERT INTO district
-    (district_name, state_id, cases, cured, active, deaths)
-    VALUES (?, ?, ?, ?, ?, ?)
-  `;
-
-  await db.run(createDistrictQuery, [
-    districtName,
-    stateId,
-    cases,
-    cured,
-    active,
-    deaths,
-  ]);
-
-  res.json("District Successfully Added");
-});
-
-// API 5: Get District by District ID
-app.get("/districts/:districtId", authenticateUser, async (req, res) => {
-  const { districtId } = req.params;
-  const selectDistrictQuery = "SELECT * FROM district WHERE district_id = ?";
-  const district = await db.get(selectDistrictQuery, [districtId]);
-  if (!district) {
-    res.status(404).json("District not found");
-    return;
+app.put(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const districtDetails = request.body;
+    const {
+      district_name,
+      state_id,
+      cases,
+      cured,
+      active,
+      deaths,
+    } = districtDetails;
+    const updateDistrictQuery = `
+    UPDATE
+      district
+    SET
+      districtName='${district_name}',
+      stateId=${state_id},
+      cases=${cases},
+      cured=${cured},
+      active=${active},
+      deaths=${deaths},
+    WHERE
+      district_id = ${districtId};`;
+    await db.run(updateDistrictQuery);
+    response.send("District Details Updated");
   }
-  res.json(district);
-});
+);
+
+//API 8
+
+app.get(
+  "/states/:stateId/stats/",
+  authenticateToken,
+  async (request, response) => {
+    const { stateId } = request.params;
+    const getStateQuery = `
+            SELECT
+              *
+            FROM
+              state
+            WHERE 
+              state_id= ${stateId};`;
+    const stateArray = await db.get(getStateQuery);
+    response.send(convertDirectorDbObjectToResponseObject(stateArray));
+  }
+);
 
 module.exports = app;
